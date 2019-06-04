64ae639011eba77cfc20fa3da32cb69724992e6a
feynmanxiong <463175047@qq.com>
2019-06-04 23:53:42 +0800

    student page of home done

--- src/js/Controller/student/s_index.js
+++ src/js/Controller/student/s_index.js
@@ -2,12 +2,14 @@
 require.config({
   paths: {
     jquery: '/lib/jquery.min-1.11.3',
-    swiper: '../../../lib/swiper-4.1.6/js/swiper.min'
+    swiper: '../../../lib/swiper-4.1.6/js/swiper.min',
+    tpl: '../../tmpl/student',
+    cookies: '../../../lib/js.cookie'
 
   }
 });
 //进行入口处理
-require(['jquery', 'swiper'], function($, Swiper) {
+require(['jquery', 'swiper', 'tpl', 'cookies'], function($, Swiper, tpl, Cookies) {
   $(function() {
     var mySwiper = new Swiper('.swiper-container', {
       autoplay: true, //可选选项，自动滑动
@@ -16,6 +18,23 @@ require(['jquery', 'swiper'], function($, Swiper) {
       loop: true,
       pagination: {
         el: '.swiper-pagination'
+      }
+    });
+    var userData = {};
+    $.ajaxSetup({headers: {
+      'Authorization': Cookies.get('Authorization')
+    }});
+    var id = Cookies.get('userID');
+    $.ajax({
+      url: 'http://n.hamkd.com/api/user/' + id,
+      type: 'GET',
+      data: '',
+      dataType: 'json',
+      success: function(response) {
+        userData.name = response.name;
+        userData.avatar = response.avatar;
+        console.log(response);
+        $('#s_header').html(tpl('Header', userData));
       }
     });
   });
