961223a1a586e395a7a14771844789d61be185f3
feynmanxiong <463175047@qq.com>
2019-06-06 00:18:02 +0800

    page of course of student is coding

---
+++ src/js/Controller/student/s_common.js
@@ -0,0 +1,20 @@
+require.config({
+  paths: {
+    jquery: '/lib/jquery.min-1.11.3'
+  }
+});
+
+require(['jquery'], function($) {
+  $(function() {
+    var aList = $('.xiong');
+    console.log(aList);
+    aList.click(function(event) {
+      // event.preventDefault();
+      for (var i = 0; i < aList.length; i++) {
+        aList[i].className = 'xiong';
+      }
+      event.target.className = 'xiong color';
+    });
+    console.log('xiongwencheng is sb');
+  });
+});
